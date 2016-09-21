9e3eda15610569ded7dcad064e4455cb0fa7bce6
Douglas Burns <db0268@bristol.ac.uk>
2016-09-21 12:22:45 +0100

    global variables in framework

---
+++ global_vars.py
@@ -0,0 +1,17 @@
+'''
+Any global variables can go here
+	import global_vars as g
+	g.GLOBAL_VARIABLE
+
+e.g. 
+Number simulated events
+Beam intensity etc
+'''
+# Number of iterations i.e. length of simulation
+N_MIPS=10
+
+
+AVE_TIME_FOR_STRIP_HIT=100
+
+AVE_ENERGY_FOR_STRIP_HIT=100
+SIGMA_ENERGY_FOR_STRIP_HIT=10
