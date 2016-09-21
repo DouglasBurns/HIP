ba81cf9731964faaf587c9e6d1a9c5a5538cf737
Douglas Burns <db0268@bristol.ac.uk>
2016-09-21 12:23:37 +0100

    get random landau and poisson numbers

---
+++ mathematical_tools.py
@@ -0,0 +1,34 @@
+'''
+Any useful mathematical tool should go here
+'''
+import numpy as np
+from ROOT import gRandom
+gRandom.SetSeed()
+# Cache for quicker running
+landau = gRandom.Landau
+poisson = gRandom.Poisson
+
+
+def return_rnd_Poisson(mu):
+	'''
+	Returning a random poisson number
+			lambda^{k} . e^{-lambda}
+	Po() =	------------------------
+					   k!
+
+	k 		: events
+	lambda 	: expected separation
+	'''
+   	rnd_po = poisson( mu )
+	return rnd_po
+
+
+def return_rnd_Landau(mu, sigma):
+	'''
+	Returning a random landau number
+
+	mu 		: location value (Not most probable value)
+	sigma 	: scale parameter (Not the standard deviation as this is not defined)
+	'''
+   	rnd_ld = landau( mu, sigma )
+	return rnd_ld
