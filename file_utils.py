c859beb3dbba31f98d16d55c52adc79dae6306e4
Douglas Burns <db0268@bristol.ac.uk>
2016-09-23 15:23:22 +0100

    added if filepath doesnt exist, create it function

--- file_utils.py
+++ file_utils.py
@@ -1,4 +1,5 @@
 import pprint
+import os
 
 def p_dict(title, dictionary):
 	pp = pprint.PrettyPrinter(indent=4)
@@ -8,3 +9,11 @@ def p_dict(title, dictionary):
 	print "-"*50
 	pp.pprint(dictionary)
 	return
+
+def make_folder_if_not_exists(folder):
+    if not os.path.exists(folder):
+        try:
+            os.makedirs(folder)
+        except:
+            print "Could not create a folder ", folder
+        
