eb357e28949df82aabe6fd7cf78d6b9901dc4af1
Douglas Burns <db0268@bristol.ac.uk>
2017-03-15 09:59:38 +0000

    Fix response bug. add electronic noise

--- mathematical_tools.py
+++ mathematical_tools.py
@@ -5,11 +5,14 @@ from __future__ import division
 
 import math
 from ROOT import gRandom, TRandom3
+import global_vars as g
 gRandom = TRandom3()
 gRandom.SetSeed(0)
 # Cache for quicker running
 landau = gRandom.Landau
 poisson = gRandom.Poisson
+uniform = gRandom.Uniform
+gaussian = gRandom.Gaus
 
 
 def return_rnd_Poisson(mu):
@@ -25,6 +28,15 @@ def return_rnd_Poisson(mu):
    	rnd_po = poisson( mu )
 	return rnd_po
 
+def return_rnd_Gaussian(mu, sigma):
+	'''
+	Returning a random gaussian number
+
+	mu 		: average
+	sigma 	: dtandard deviation
+	'''
+   	rnd_gs = gaussian( mu, sigma )
+	return rnd_gs
 
 def return_rnd_Landau(mu, sigma):
 	'''
@@ -87,42 +99,58 @@ def amplifier_response(new_q, baseline_v):
 	Return the response of the amplifier
 	Response is ~ linear until 139fC
 	Max linear voltage ~ 633mV at q=139 fC
-	Max voltage readout ~ 717mV for q>139 fC
+	Max voltage readout ~ 717mV for q>>139 fC
 
 	V[mV] = 5.02*q - 0.00333*pow(q,2)    		q<139 fC
 	V[mV] = 717 - 83.5*math.exp(-(q-139)/75.5) 	q>139 fC
 
 	Response as given by Geoff Hall and Mark Raymond
 	'''
-	max_linear_range = 139
-	new_v = 0
-	signal_q = 0
-
+	max_linear_range 	= 139
+	new_v 				= 0
+	signal_q 			= 0
+	bkg_electronic 		= 0
 	# Finding the baseline charge in chip in fC
 	# Solution to the response equation
 	if baseline_v < 633:
 		baseline_q = (-5.02+math.sqrt(pow(5.02,2)-4*0.00333*baseline_v)) / (-2*0.00333)
 		signal_q = baseline_q + new_q
+		# Add electronic noise
+
 		# Now put back into response equation
 		# linear
+		# This can be -ve for very verylarge q
 		new_v = 5.02*signal_q - 0.00333*(pow(signal_q,2))
-		# baseline_v = 5.02*signal_q - 0.00333*pow(baseline_q,2)
-		gain_v = new_v - baseline_v
+
 		# nonlinear
-		if gain_v > 633:
-			gain_v = 717 - 83.5*math.exp(-(signal_q-139)/75.5)
-	else:
-		if baseline_v<717:
-			baseline_q = max_linear_range - 75.5*math.log((717-baseline_v) / 83.5)
-			signal_q = baseline_q + new_q
-			# again put back into response equation
+		if signal_q > 139:
 			new_v = 717 - 83.5*math.exp(-(signal_q-max_linear_range)/75.5)
-			# baseline_v = 717 - 83.5*math.exp(-(baseline_q-max_linear_range)/75.5))
-			gain_v = new_v - baseline_v
-		else:
-			# No gain for any signal with the baseline currently above the maximum
-			gain_v = 0
-	return gain_v, new_v, signal_q
+
+		bkg_electronic = return_rnd_Gaussian(0, g.AVERAGE_ELECTRONIC_NOISE)
+		new_v += bkg_electronic
+		# add noise
+		gain_v = new_v - baseline_v
+
+	elif baseline_v<717:
+		baseline_q = max_linear_range - 75.5*math.log((717-baseline_v) / 83.5)
+		signal_q = baseline_q + new_q
+		# again put back into response equation
+		new_v = 717 - 83.5*math.exp(-(signal_q-max_linear_range)/75.5)
+		bkg_electronic = return_rnd_Gaussian(0, g.AVERAGE_ELECTRONIC_NOISE)
+		new_v += bkg_electronic
+
+		gain_v = new_v - baseline_v
+	else:
+		# No gain for any signal with the baseline currently above the maximum
+		gain_v = 0
+		# if baseline is higher than 717 just got to wait until it decays lower...
+		new_v = 717
+
+	# Stop -ve signals
+	if new_v < 0: 
+		gain_v = 0
+		new_v = 0
+	return gain_v, new_v, signal_q, bkg_electronic
 
 def is_beam_present(clock_cycle):
 	'''
@@ -188,6 +216,7 @@ def is_beam_present(clock_cycle):
 	if( (mod_clock_cycle > 3285) and (mod_clock_cycle <= 3293) ) : is_beam = False
 	if( (mod_clock_cycle > 3365) and (mod_clock_cycle <= 3373) ) : is_beam = False
 	if( (mod_clock_cycle > 3445) and (mod_clock_cycle <= 3564) ) : is_beam = False
+	# if( mod_clock_cycle > 800 ) : is_beam = False
 
 	return is_beam
 
@@ -221,25 +250,25 @@ def tracker_hits(occupancy, particles_in_bx, ave_particles_in_bx ):
 	n_tracker_hits = return_rnd_Poisson( p_strip_hit )
 	return n_tracker_hits
 
+def is_HIP(HIPocc):
+	'''
+	Is a MIP hit a HIP?
+	'''
+	if (uniform(0,1) < HIPocc):
+		return True
+	else:
+		return False
 
+def return_HIP_charge():
+	'''
+	Assign energy of 0.1-20 MeV randomly
+	'''
+	energy_HIP = uniform(0.1,20)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	# MeV -> eV : * math.pow(10,6)
+	# eV -> e 	: / 3.6 ?????
+	charge_HIP = energy_HIP * math.pow(10,6) / 3.6
+	# print "Charge on HIP : ", charge_HIP
+	return charge_HIP
 
 
